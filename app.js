9b83bb305f33f0a88cf4bc80f077125d4de15266
mikhailidi <mikhailidi.p@gmail.com>
2020-08-12 17:59:52 +0200

    ♻️group routes together

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ app.use(express.json());
 app.get('/', routeHandler.home);
 
 app.get('/api/v1/tours', routeHandler.getTours);
-app.get('/api/v1/tours/:id', routeHandler.getTour);
-app.patch('/api/v1/tours/:id', routeHandler.updateTour);
-app.delete('/api/v1/tours/:id', routeHandler.deleteTour);
+app
+  .route('/api/v1/tours')
+  .get(routeHandler.getTours)
+  .post(routeHandler.addTour);
 
-app.post('/api/v1/tours', routeHandler.addTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(routeHandler.getTour)
+  .patch(routeHandler.updateTour)
+  .delete(routeHandler.deleteTour);
 
 app.listen(port, () => {
   console.log(`Natour app started on port ${port}`);
