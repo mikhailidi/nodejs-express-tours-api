34d60260fe456c61e551ff0f1c5c364c754954a0
mikhailidi <mikhailidi.p@gmail.com>
2020-08-12 19:00:43 +0200

    ♻️refactor to multiple route handlers

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const app = express();
-const routeHandler = require('./lib/handlers');
+const routeHandler = require('./lib/routeHandlers/handler');
 const port = 3000;
 
 app.use(express.json());
 
 app.get('/', routeHandler.home);
 
-app.get('/api/v1/tours', routeHandler.getTours);
-app
-  .route('/api/v1/tours')
-  .get(routeHandler.getTours)
-  .post(routeHandler.addTour);
-
-app
-  .route('/api/v1/tours/:id')
-  .get(routeHandler.getTour)
-  .patch(routeHandler.updateTour)
-  .delete(routeHandler.deleteTour);
+app.use('/api/v1/tours', routeHandler.toursHandler);
+app.use('/api/v1/users', routeHandler.usersHandler);
 
 app.listen(port, () => {
   console.log(`Natour app started on port ${port}`);
