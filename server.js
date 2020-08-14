3a91102d40fa82953e1e0d88fb77ecf10f137472
mikhailidi <mikhailidi.p@gmail.com>
2020-08-14 18:50:32 +0200

    ♻️use port from .env file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const dotenv = require('dotenv');
 const app = require('./app');
-const port = 3000;
 
 dotenv.config({
   path: './.env',
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Natour app started on port ${port}`);
 });
