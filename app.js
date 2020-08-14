58599a4880f1f4c1486eddf60a27891ad142bf1d
mikhailidi <mikhailidi.p@gmail.com>
2020-08-14 18:33:18 +0200

    ✨add static middleware

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const routeHandler = require('./lib/routeHandlers/handler');
 
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', routeHandler.home);
 app.use('/api/v1/tours', routeHandler.toursHandler);
