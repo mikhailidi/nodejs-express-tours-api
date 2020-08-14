9c05290a53d2e101bfae9010ef3ea6269bcdb5a9
mikhailidi <mikhailidi.p@gmail.com>
2020-08-14 18:45:27 +0200

    ✨configure & use dotenv

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 const port = 3000;
+
+dotenv.config({
+  path: './.env',
+});
 
 app.listen(port, () => {
   console.log(`Natour app started on port ${port}`);
