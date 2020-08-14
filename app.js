8b0fd1f4e11c48b18c4b79a3eabdbc067b9a95a4
mikhailidi <mikhailidi.p@gmail.com>
2020-08-14 14:57:52 +0200

    ♻️refactor to separated server.js & app.js

--- app.js
+++ app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const app = express();
 const routeHandler = require('./lib/routeHandlers/handler');
-const port = 3000;
 
 app.use(express.json());
 
 app.get('/', routeHandler.home);
-
 app.use('/api/v1/tours', routeHandler.toursHandler);
 app.use('/api/v1/users', routeHandler.usersHandler);
 
-app.listen(port, () => {
-  console.log(`Natour app started on port ${port}`);
-});
+module.exports = app;
