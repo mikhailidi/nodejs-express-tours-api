e67125b63861cc618f3e9c6cc2fa04cd5d6f24f6
mikhailidi <mikhailidi.p@gmail.com>
2020-08-21 16:02:02 +0300

    🗃 add a connection to mongoDB

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({
   path: './.env',
 });
+
+const DB = process.env.DB_CONNECTION.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
